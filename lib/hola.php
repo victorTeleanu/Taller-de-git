2320bd9a1016b240db761aa10dbee61c84f59cf6
Victor Teleanu <alexteleanu9@gmail.com>
2024-09-23 12:01:28 +0200

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
-<?php
+X<?php
 // Autor: Victor Teleanu <alexteleanu9@gmail.com>
-print "Introduce tu nombre:";
+require('HolaMundo.php');
+
+print('Introduce tu nombre:');
 $nombre = trim(fgets(STDIN));
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
 ?>
